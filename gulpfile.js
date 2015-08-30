0fc7b903c05bad55d50de0a8deec88b05b7865ab
Jeesun Kim <jeesunikim@gmail.com>
2015-08-30 16:12:40 -0400

    superdesk > raphael and angular added

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	minifyCSS = require('gulp-minify-css'),
+	uglify = require('gulp-uglify'),
+	watch = require('gulp-watch')
+	rename = require('gulp-rename');
+
+gulp.task('sass', function() {
+	gulp.src('./src/scss/**/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(minifyCSS())
+	.pipe(rename('style.min.css'))
+	.pipe(gulp.dest('./dest/css'));
+  // place code for your default task here
+});
+
+gulp.task('compress', function() {
+	gulp.src('./src/js/**/*.js')
+	.pipe(uglify())
+	.pipe(gulp.dest('./dest/js'));
+})
+
+gulp.task('sass:watch', function () {
+	gulp.watch('./src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('watch', function () {
+	gulp.watch('./src/js/**/*.js', ['compress']);
+	gulp.watch('./src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'compress', 'watch']);
---
+++ server/app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// var routes = require('../routes/index');
+// var users = require('../routes/users');
+
+var app = express();
+
+// The path of our index.html file
+var indexHtmlPath = path.join(__dirname, '../index.html');
+var publicPath = path.join(__dirname, '../dest');
+// view engine setup
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(publicPath));
+
+// app.use('/', routes);
+// app.use('/users', users);
+
+app.get('/', function (req, res) {
+  res.sendFile(indexHtmlPath);
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+module.exports = app;
